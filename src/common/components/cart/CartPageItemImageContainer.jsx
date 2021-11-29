7bf76db6f1d0bb176268155221fbb60684839e80
Erikyy <erikyl@hotmail.com>
2021-11-29 21:48:15 +0200

    Add cart page gallery image switching buttons

--- src/common/components/cart/CartPageItemImageContainer.jsx
+++ src/common/components/cart/CartPageItemImageContainer.jsx
@@ -1,9 +1,12 @@
 import React from 'react';
 import styled from 'styled-components';
+import { ChevronLeft, ChevronRight } from '../../../icons/Icons';
+import './styles/CartPageItemImageContainer.css';
 
 const Wrapper = styled.div`
   width: 300px;
   padding: 5px;
+  position: relative;
 `;
 
 const Image = styled.img`
@@ -14,10 +17,51 @@ const Image = styled.img`
 `;
 
 export default class CartPageItemImageContainer extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      galleryIndex: 0,
+    };
+  }
+
+  decremntGalleryIndex() {
+    let index = this.state.galleryIndex;
+    index -= 1;
+    if (index === -1) {
+      index = this.props.data.length - 1;
+    }
+    this.setState(() => ({ galleryIndex: index }));
+  }
+
+  incrementGalleryIndex() {
+    let index = this.state.galleryIndex;
+    index += 1;
+    if (index === this.props.data.length) {
+      index = 0;
+    }
+    this.setState(() => ({ galleryIndex: index }));
+  }
+
   render() {
     return (
       <Wrapper>
-        <Image src={this.props.data[0]} alt="img" />
+        <div className="image-gallery-button-container">
+          <button
+            className="image-gallery-button"
+            type="button"
+            onClick={() => this.decremntGalleryIndex()}
+          >
+            <ChevronLeft />
+          </button>
+          <button
+            className="image-gallery-button"
+            type="button"
+            onClick={() => this.incrementGalleryIndex()}
+          >
+            <ChevronRight />
+          </button>
+        </div>
+        <Image src={this.props.data[this.state.galleryIndex]} alt="img" />
       </Wrapper>
     );
   }
